dba2a62a27c3eb2fed2d5fcaf3748fa9a13f50da
bugatsinh <92563127+bugatsinh@users.noreply.github.com>
2021-12-26 20:36:54 +0530

    importent
    this is importent for me

--- packageharry/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-setup(name="packageharry",
-version="0.4",
-description="this is cood with harry package",
-long_description="this is a very very long description",
-auther="harry",
-packages=["packageharry"],
-install_requires=[])
